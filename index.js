903b0e97959986c44166c8ba6bd178a4bc55323a
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-01-28 17:15:37 +1300

    Rebuild

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ morgan.token('body', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let contacts = [
     {
